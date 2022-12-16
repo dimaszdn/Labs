512f8f289a41197e822540bf834fa0acbb02d662
dimaszdn <dimaszdn@mail.ru>
2022-12-16 13:25:36 +0200

    1 лабораторная

---
+++ Lab-1/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	
+	std::cout << "Введите длину первой стороны: ";
+	int a;
+	std::cin >> a;
+
+	std::cout << "Введите длину второй стороны: ";
+	int b;
+	std::cin >> b;
+
+	std::cout << "Введите значение угла: ";
+	double angle;
+	std::cin >> angle;
+	angle = angle * (acos(-1) / 180);
+
+	double S;
+	S = 0.5 * a * b * sin(angle);
+	std::cout << "S = " << S << std::endl;
+
+	return 0;
+}
